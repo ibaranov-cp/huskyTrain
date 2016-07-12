4cf4d5eaf855b8579f1f5709337d7f8dbe475f68
ibaranov-cp <ibaranov@clearpathrobotics.com>
2016-07-12 19:10:08 -0400

    Added basics of recording position, localization is ok

--- src/gen_follow.py
+++ src/gen_follow.py
@@ -2,25 +2,47 @@
 
 import roslib
 import rospy
+import pickle
 from geometry_msgs.msg import Twist
 from std_msgs.msg import Bool
 from sensor_msgs.msg import Joy
+from geometry_msgs.msg import PoseStamped
+from nav_msgs.msg import Odometry
 from ddynamic_reconfigure_python.ddynamic_reconfigure import DDynamicReconfigure
 
 mode_flag = 0
+pose_array = []
 
 def mode(data):
     global mode_flag
+    global pose_array
     if data.buttons[5] == 1:
-        rospy.loginfo("Recording!")
+        #rospy.loginfo("Recording!")
         mode_flag = 1
-    if data.buttons[4] == 1:
-        rospy.loginfo("Playback!")
+    elif data.buttons[4] == 1:
+        #pose_array = pickle.load(open("poses.p","rb"))
+        #rospy.loginfo("Playback!")
         mode_flag = 2
+        print pose_array
+    else:
+        #if mode_flag == 1: pickle.dump(pose_array,open( "poses.p", "wb" ))
+        mode_flag = 0
 
 def dyn_rec_callback(config, level):
     rospy.loginfo("Received reconf call: " + str(config))
     return config
+
+# Nasty rate limit hack
+rate = 0
+
+def monitor(data):
+    global pose_array
+    global rate
+    if mode_flag ==1:
+        rate = rate + 1
+    if rate >= 80:
+        rate = 0
+        pose_array.append(data)
 
 def looper():
     rospy.init_node('gen_follow', anonymous=True)
@@ -36,8 +58,9 @@ def looper():
     # Start DDynamic reconfigure server
     ddynrec.start(dyn_rec_callback)
 
-    #Listen to the controller
+    #Listen to the controller and robot_localization data
     rospy.Subscriber("/joy_teleop/joy", Joy, mode)
+    rospy.Subscriber("/odometry/filtered", Odometry, monitor)
 
     #Main Loop
     rate = rospy.Rate(10) # 10hz
